9cf606cfb907805a29f105bff80bb89e71baa0f0
Yamaguchi Takuya <yamat47.thirddown@gmail.com>
2021-12-31 20:03:16 +0900

    Add reference from prefecture to city.

--- prefecture.rb
+++ prefecture.rb
@@ -1,8 +1,25 @@
 # frozen_string_literal: true
 
+require 'csv'
+
+require './city'
+
 class Prefecture
   attr_reader :code, :name, :name_kana, :name_romaji
   attr_accessor :cities
+
+  class << self
+    def all
+      CSV.table('./parsed_data/prefectures.csv').map do |prefecture|
+        new(
+          code: prefecture[:code],
+          name: prefecture[:name],
+          name_kana: prefecture[:name_kana],
+          name_romaji: prefecture[:name_romaji]
+        )
+      end
+    end
+  end
 
   def initialize(code:, name:, name_kana:, name_romaji:)
     @code = code
@@ -14,5 +31,16 @@ class Prefecture
 
   def formatted_code
     format("%02<number>d", number: code)
+  end
+
+  def cities
+    CSV.table("./parsed_data/#{formatted_code}.csv").map do |city|
+      City.new(
+        code: city[:code],
+        name: city[:name],
+        name_kana: city[:name_kana],
+        name_romaji: city[:name_romaji]
+      )
+    end
   end
 end
